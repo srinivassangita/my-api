a1bafbde4f76cdd6906ede1ca833e1bbd0f9c6a1
srinivassangita <61300959+srinivassangita@users.noreply.github.com>
2021-12-15 17:44:01 -0600

    Add files via upload

---
+++ Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvsTaskManager
+{
+  public class Project
+  {
+    [Key]
+    [DatabaseGenerated (DatabaseGeneratedOption.None)]
+    public int ProjectID { get; set; }
+    public string ProjectName { get; set; }
+    public DateTime DateOfStart { get; set; }
+    public int TeamSize { get; set; }
+  }
+
+  public class TaskManagerDbContext:DbContext
+  {
+    public DbSet<Project> Projects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      base.OnConfiguring(optionsBuilder);
+      //optionsBuilder.UseSqlServer("data source = localhost; integrated security = yes; initial catalog = TaskManager");
+      optionsBuilder.UseSqlServer(@"data source=(localdb)\MSSQLLocalDB;integrated security=True;initial catalog = Hero");
+
+    }
+  }
+}
